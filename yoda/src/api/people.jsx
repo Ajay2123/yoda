5478282fd121be2afe890b14be18efaad0fe3a54
SaiDivya <bsaidivya97@gmail.com>
2024-03-13 20:38:54 +0100

    Api calls

---
+++ yoda/src/api/people.jsx
@@ -0,0 +1,9 @@
+const API_BASE_URL = "https://yoda-backend.vercel.app/api";
+
+export const fetchPeople = async () =>
+    fetch(`${API_BASE_URL}/people`)
+        .then((result) => result.json())
+        .catch((error) => {
+            console.log(error);
+            return error;
+        });
